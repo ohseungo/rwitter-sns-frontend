222b70c4f2ac6a3a0d5ddad7ec980a69dc2f8d4d
OH SW <ohseungo@naver.com>
2022-07-10 17:27:32 +0900

    feat : Header 생성

--- src/components/common/Header.styled.ts
+++ src/components/common/Header.styled.ts
@@ -1,1 +1,6 @@
 import styled from "@emotion/styled";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+
+export const HeaderRoot = styled(AppBar)``;
+export const LogoText = styled(Typography)``;
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,11 @@
-const Header = () => {};
+import { HeaderRoot, LogoText } from "./Header.styled";
+
+const Header = () => {
+  return (
+    <HeaderRoot>
+      <LogoText>Hello</LogoText>
+    </HeaderRoot>
+  );
+};
 
 export default Header;
