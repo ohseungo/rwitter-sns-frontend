a62b7328a67c13034df80a9f04455ee1d519cb6f
OH SW <ohseungo@naver.com>
2022-07-20 19:29:42 +0900

    feat : Main Page div

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,11 @@
+import { Root } from "./Home.styled";
+
 const HomePage = () => {
-  return <div>Hello Home</div>;
+  return (
+    <Root>
+      <div>Hello Home</div>
+    </Root>
+  );
 };
 
 export default HomePage;
