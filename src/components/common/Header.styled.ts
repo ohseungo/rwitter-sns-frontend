518f67ee14e564510c740b6e349df5ab93e8019f
OH SW <ohseungo@naver.com>
2022-07-23 21:38:31 +0900

    feat : header Link 추가 및 메뉴 간격 조절

--- src/components/common/Header.styled.ts
+++ src/components/common/Header.styled.ts
@@ -1,8 +1,13 @@
 import styled from "@emotion/styled";
 import AppBar from "@mui/material/AppBar";
-import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 
-export const HeaderRoot = styled(AppBar)``;
-export const LogoText = styled(Typography)`
+export const HeaderRoot = styled(AppBar)`
+  a {
+    color: #000000;
+    text-decoration: none;
+  }
+`;
+export const LogoLink = styled(Link)`
   flex: 1;
 `;
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,16 @@
+import { Typography } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
-import { Link } from "react-router-dom";
-import { HeaderRoot, LogoText } from "./Header.styled";
+import { HeaderRoot, LogoLink } from "./Header.styled";
 import ProfileButton from "./ProfileButton";
+import { Link } from "react-router-dom";
 
 const Header = ({ setLoggedIn }: { setLoggedIn: Function }) => {
   return (
     <HeaderRoot color="transparent" position="sticky" elevation={0}>
       <Toolbar>
-        <LogoText variant="h6">Rwitter</LogoText>
+        <LogoLink to="/">
+          <Typography variant="h6">Rwitter</Typography>
+        </LogoLink>
         <Link to="/profile">
           <ProfileButton />
         </Link>
