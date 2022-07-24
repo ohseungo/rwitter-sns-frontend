b372f3ac6e5a4dbdc059177cc6910e2709c58fe3
OH SW <ohseungo@naver.com>
2022-07-24 21:47:52 +0900

    feat : 모듈화 form 적용

--- src/components/profile/ProfileEditForm.styled.ts
+++ src/components/profile/ProfileEditForm.styled.ts
@@ -1,3 +1,11 @@
 import styled from "@emotion/styled";
+import TextField from "@mui/material/TextField";
+import { Form as ManagedForm } from "components/common/StyledComponent";
 
-export const Root = styled.div``;
+export const Root = styled(ManagedForm)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NicknameInput = styled(TextField)``;
--- src/components/profile/ProfileEditForm.tsx
+++ src/components/profile/ProfileEditForm.tsx
@@ -1,9 +1,13 @@
 import { Root } from "./ProfileEditForm.styled";
+import React, { useCallback } from "react";
 
 const ProfileEditForm = () => {
+  const handleSubmit = useCallback((event: React.FormEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  }, []);
   return (
     <>
-      <Root>Hello Edit Form</Root>
+      <Root onSubmit={handleSubmit}>Hello Edit Form</Root>
     </>
   );
 };
