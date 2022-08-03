9af6a76ac90e11a1fd94ff7fb5e001d94d6d0d99
OH SW <ohseungo@naver.com>
2022-08-03 21:42:11 +0900

    feat : list import 추가

--- src/components/profile/FollowingList.tsx
+++ src/components/profile/FollowingList.tsx
@@ -1,3 +1,10 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+
 const FollowingList = () => {};
 
 export default FollowingList;
