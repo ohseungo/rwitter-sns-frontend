be9c46f5aedb9a12c1d6e8def99fa73778f6eb38
OH SW <ohseungo@naver.com>
2022-08-07 17:21:57 +0900

    feat : typography test

--- src/pages/profile/Profile.styled.ts
+++ src/pages/profile/Profile.styled.ts
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 export const Root = styled(Box)`
   padding: 1.5rem;
   border: 1px solid black;
 `;
+
+export const ProfileText = styled(Typography)``;
