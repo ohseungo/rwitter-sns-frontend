40cd6449ea8e79d3a103293a0d8428724ee6a88e
OH SW <ohseungo@naver.com>
2022-08-08 20:48:37 +0900

    feat : 간단한 텍스트 추가

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,12 +1,13 @@
 import UserList from "components/profile/UserList";
 import ProfileEditForm from "components/profile/ProfileEditForm";
-import { Root } from "./Profile.styled";
+import { ProfileText, Root } from "./Profile.styled";
 
 const ProfilePage = () => {
   const dummyUserList = ["친구", "친구아님"];
   return (
     <Root>
       <ProfileEditForm />
+      <ProfileText>팔로잉</ProfileText>
       <UserList userList={dummyUserList} />
     </Root>
   );
