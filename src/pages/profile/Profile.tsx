3e558d2bc11b2efe67173ce07a4b5944cd4db227
OH SW <ohseungo@naver.com>
2022-08-12 21:49:45 +0900

    feat : tab 적용 테스트

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -2,6 +2,9 @@ import UserList from "components/profile/UserList";
 import ProfileEditForm from "components/profile/ProfileEditForm";
 import { ProfileText, Root } from "./Profile.styled";
 
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
 const ProfilePage = () => {
   const dummyUserList = ["친구", "친구아님"];
   return (
