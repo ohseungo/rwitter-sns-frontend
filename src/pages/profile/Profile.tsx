269a5576fd409140f6481f40256b5bd6ba77854c
OH SW <ohseungo@naver.com>
2022-08-17 22:50:11 +0900

    tab profile 기본 적용

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,14 +4,43 @@ import { ProfileText, Root } from "./Profile.styled";
 
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import { useState } from "react";
+import TabPanel from "components/base/TabPanel";
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const ProfilePage = () => {
   const dummyUserList = ["친구", "친구아님"];
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: any, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <Root>
       <ProfileEditForm />
       <ProfileText>팔로잉</ProfileText>
-      <Tabs></Tabs>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel selectedIndex={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel selectedIndex={value} index={1}>
+        Item Two
+      </TabPanel>
       <UserList userList={dummyUserList} />
     </Root>
   );
