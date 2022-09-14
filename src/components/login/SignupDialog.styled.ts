74267d997696cb2e6dffda59d7c9ef11333ea042
OH SW <ohseungo@naver.com>
2022-09-14 23:01:34 +0900

    feat : Signup dialog 중간

--- src/components/login/SignupDialog.styled.ts
+++ src/components/login/SignupDialog.styled.ts
@@ -1,4 +1,7 @@
 import styled from "@emotion/styled";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 export const Root = styled(Box)``;
+
+export const Title = styled(Typography)``;
