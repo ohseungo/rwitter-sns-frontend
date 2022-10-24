e55f631832401284579e51f160afce283c4d09ac
OH SW <ohseungo@naver.com>
2022-10-24 23:18:34 +0900

    feat : redux devtools ext 설치

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, compose, configureStore } from "@reduxjs/toolkit";
 import rootReducer from "store/reducers";
 
+const middlewares: any[] = [];
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(...middlewares))
+    : compose(applyMiddleware(...middlewares));
 const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(
