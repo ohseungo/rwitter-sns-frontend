36f4020b8b71c05de485ed50ef73f18c629da907
OH SW <ohseungo@naver.com>
2022-10-24 23:21:46 +0900

    feat : redux devtools ext 제거 - toolit에 이미 포함

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { applyMiddleware, compose, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "store/reducers";
 
-const middlewares: any[] = [];
-const enhancer =
-  process.env.NODE_ENV === "production"
-    ? compose(applyMiddleware(...middlewares))
-    : compose(applyMiddleware(...middlewares));
 const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(
